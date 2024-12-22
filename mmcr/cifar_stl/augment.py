037d2e7453cd81797bf8558271965e6d2f23abb8
as821 <andrew.r.stange@gmail.com>
2024-12-21 18:27:00 -0800

    stash

--- mmcr/cifar_stl/augment.py
+++ mmcr/cifar_stl/augment.py
@@ -30,6 +30,8 @@ def bernoulli_aug(aug, orig, prob):
 
 def generate_aug_probs(img_shape, device):
     def horiz_vert_trans_operator(img_shape, device):
+        unif_range = 7
+        
         sz = img_shape[-2] * img_shape[-1]
         M = torch.zeros((sz, sz))        
         for x in range(img_shape[-2]):
@@ -37,7 +39,6 @@ def generate_aug_probs(img_shape, device):
                 m_idx = x * img_shape[-2] + y
                 
                 # NOTE: assume uniform dist over some max translation for both horizontal and vertical for initial simplicity
-                unif_range = 3
                 for x_off in range(-1 * unif_range, unif_range):
                     if x_off + x < 0 or x_off + x >= img_shape[-1]:
                         continue
@@ -50,7 +51,6 @@ def generate_aug_probs(img_shape, device):
                         M[m_idx, m_off_idx] = (1 / (unif_range * 2 + 1)) ** 2
         
         # a "1" for each location in the image. this filter extracts the (patch_sz, patch_sz) patch centered at each pixel in the image (with zero padding)
-        unif_range = 3
         patch_sz = unif_range * 2 + 1
         inp_chan = 3
         conv = torch.nn.Conv2d(inp_chan, patch_sz * patch_sz * inp_chan, patch_sz, stride=1, padding=unif_range, bias=False).to(device)
@@ -185,6 +185,9 @@ def generate_aug_probs(img_shape, device):
         # M is the same across all channels (and channels are independent of one another)
         return torch.block_diag(*[M for _ in range(img_shape[0])])
 
+    zoom_factor = 1
+    img_shape = (img_shape[0], int(img_shape[1] * zoom_factor), int(img_shape[2] * zoom_factor))
+
     trans_op, conv = horiz_vert_trans_operator(img_shape, device)
     return {"horiz_vert_trans" : trans_op, "horiz_vert_conv":conv, "zoom":zoom_operator(img_shape)}
 
@@ -232,7 +235,7 @@ def calc_aug_ev_var(x, prob_map):
         
         # second moment is the convolution of the windows of possible translations for a pair of pixels, weighted by the probability of those translations
         with torch.no_grad():
-            unif_range = 3
+            unif_range = 7
             patch_sz = unif_range * 2 + 1            
             inp_chan = 3
             conv = prob_map["horiz_vert_conv"]
@@ -241,16 +244,17 @@ def calc_aug_ev_var(x, prob_map):
             patches = conv(batch).flatten(2)
             
             # take (probability-weighted) convolution of all pairs of pixels to get the second moment
-            prob = torch.full((patch_sz * patch_sz * inp_chan,), 1 / (patch_sz * patch_sz), device=patches.device)
+            prob = torch.full((inp_chan, patch_sz * patch_sz), 1 / (patch_sz * patch_sz), device=patches.device)
             weighted_outer = torch.zeros((patches.shape[0], patches.shape[2], patches.shape[2], inp_chan), device=patches.device)
             for idx in range(patches.shape[0]):     # full outer product too memory intensive with larger batch sizes
                 b = patches[idx].T
-                b = b.unsqueeze(0) * b.unsqueeze(1) 
-                b = b * prob
-                b = einops.rearrange(b, "A D (I C) -> A D C I", C=inp_chan)
-                b = b.sum(dim=-1)
-                weighted_outer[idx] = b
-            
+                b = einops.rearrange(b, "A (I C) -> A C I", C=inp_chan)
+
+                # o = b.unsqueeze(0) * b.unsqueeze(1) 
+                # o = o * prob
+                # o = o.sum(dim=-1)
+                weighted_outer[idx] = torch.einsum('chw,bhw,hw->cbh', b, b, prob)
+
             # reformat so each channel of image only interacts with entries for that channel (block_diag if it supported batching)
             shp = weighted_outer.shape
             second = torch.zeros((shp[0], shp[1] * shp[3], shp[2] * shp[3]), device=weighted_outer.device)
@@ -271,13 +275,32 @@ def calc_aug_ev_var(x, prob_map):
         """
         
         # TODO(as) get expected augmentation + variance for zoom 
-        ev, zoom_var = zoom(x)
-
-        # TODO(as) get expected augmentation + variance for horizontal + vertical translations
-        # ev, trans_var = horiz_vert_trans(x)
+        # ev, zoom_var = zoom(x)
 
 
-        return ev, zoom_var
+        # NOTE: use deterministic zoom for now (effectively a fixed-size crop rather than a random sized one)
+        zoom_factor = 1
+        b, c, h, w = x.shape
+        new_h = int(h * zoom_factor)
+        new_w = int(w * zoom_factor)
+        x = torchvision.transforms.functional.resize(x, (new_h, new_w), antialias=True)
+
+        # get expected augmentation + variance for horizontal + vertical translations
+        ev, trans_var = horiz_vert_trans(x)
+        var = trans_var
+
+        # center crop back to original size
+        crop_h = int((new_h - h) / 2)
+        crop_w = int((new_w - w) / 2)        
+        ev = einops.rearrange(ev, "B (C H W) -> B C H W", H=new_h, W=new_w)
+        ev = ev[..., crop_h : crop_h + h, crop_w : crop_w + w]
+        ev = einops.rearrange(ev, "B C H W -> B (C H W)")
+
+        var = einops.rearrange(var, "B (C H W) (X Y Z) -> B C H W X Y Z", H=new_h, W=new_w, Y=new_h, Z=new_w)
+        var = var[..., crop_h : crop_h + h, crop_w : crop_w + w, :, crop_h : crop_h + h, crop_w : crop_w + w]
+        var = einops.rearrange(var, "B C H W X Y Z -> B (C H W) (X Y Z)")
+
+        return ev, var
 
     ev, rrc_var = random_resized_crop(x)
     var = rrc_var
