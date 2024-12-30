6762876c48d76037d615c055b74c1821ce71df4b
Andrew Stange <andrew.r.stange@gmail.com>
2024-12-29 16:26:41 -0800

    augmentation clean up

--- mmcr/cifar_stl/augment.py
+++ mmcr/cifar_stl/augment.py
@@ -13,121 +13,18 @@ import sys
 import pdb
 
 
-
-
-def bernoulli_aug(aug, orig, prob):
-    ev = prob * aug + (1 - prob) * orig
-
-    # covariance matrix is N x N (where N is the size of the flattened image)
-    second_mom_diag = prob * (aug**2) + (1 - prob) * (orig**2)      # E[X^2]
-    var_diag = second_mom_diag - (ev ** 2)                          # E[X^2] - E[X]^2
-
-    # in the bernoulli case all pixels are independent of one another so covariance matrix is diagonal
-    var = torch.diag(var_diag.flatten())
-    return ev, var
-
-
-
-
-
-def resize_crop_operator(img_shape, zoom_factors):
-    def _per_crop_prob_calc(M, zf, horiz_step, vert_step, h, w):
-
-        # for u in range(h):
-        #     for v in range(w):
-        #         # (u, v) is (row, col) destination crop indexing. (x, y) is (row, col) indexing of the source pixel in the original image
-        #         x = int((u + vert_step) / zf)
-        #         y = int((v + horiz_step) / zf)
-        #         m_source_idx = x * w + y
-
-        #         m_idx = u * w + v
-        #         local_M[m_idx, m_source_idx] += 1
-
-        #         for u_prime in range(h):
-        #             for v_prime in range(w):
-        #                 x_prime = int((u_prime + vert_step) / zf)
-        #                 y_prime = int((v_prime + horiz_step) / zf)
-        #                 prime_idx = x_prime * w + y_prime
-        #                 local_var[m_source_idx, prime_idx] += 1
-
-        # return local_M, local_var
-
-        # calculate the source pixel coordinates from: destination pixel coordinates, zoom amount, + horiz/vert translation
-        local_M = torch.zeros_like(M)
-        u_grid, v_grid = torch.meshgrid(torch.arange(h), torch.arange(w), indexing='ij')
-        x = ((u_grid + vert_step) / zf).int()
-        y = ((v_grid + horiz_step) / zf).int()
-        m_source_idx = (x * w + y).reshape(-1)
-        m_dest_idx = (u_grid * w + v_grid).reshape(-1)
-        
-        # NOTE: can do assignment without accumulation since each m_dest_idx is unique. this implies that each (dest, source) pair is also unique
-        local_M[m_dest_idx, m_source_idx] = 1
-
-        # Outer product of counts with itself gives us the number of times each (m_source_idx, m_source_idx) pair appears
-        # counts = torch.bincount(m_source_idx, minlength=var.shape[0])
-        # local_var = torch.outer(counts, counts)
-
-        return local_M
-
-    
-    # Discretized zoom (discretized crop sizes) allow the definition of a discrete uniform probability distribution over all crop size and horiz/vert translation pairs
-    h, w = img_shape[-2], img_shape[-1]
-    sz = h * w
-    M = torch.zeros((sz, sz)) 
-
-    cache = {}
-    for u in range(h):
-        for v in range(w):
-            cache[(u, v)] = []
-
-    # TODO(as) this is stupid. discretize the zoom. Then just define a uniform prob. dist over all possible crops (Cart. prod. of all sizes with all valid horiz/vert translations)
-    # Then, iterate through all these crops and determine which source pixel each final pixel comes from (+ associate appropriate prob. mass to it)
-    n_step = 0
-    for zf in zoom_factors:
-        # determine zoom size and then find all windows of original image size that will work as a crop
-        zoom_h, zoom_w = int(zf * h), int(zf * w)
-        n_horiz_step = zoom_w - w + 1
-        n_vert_step = zoom_h - h + 1
-
-        # iterate through all possible translations of this size of crop (assigning each uniform probability)
-        for horiz_step in tqdm(range(n_horiz_step)):
-            for vert_step in range(n_vert_step):
-                n_step += 1
-                # determine the source image pixel that corresponds to each output crop pixel and update M
-                local_M = _per_crop_prob_calc(M, zf, horiz_step, vert_step, h, w)
-                M += local_M
-
-                # prep (u, v) -> (x, y) cache for variance calculation
-                for u in range(w):
-                    for v in range(h):
-                        x = int((u + vert_step) / zf)
-                        y = int((v + horiz_step) / zf)
-                        cache[(u, v)].append((x, y))
-
-
-
-    # normalize M values by the total number of possible (crop size, horiz/vert translation) combinations
-    # NOTE: this weights all crop sizes equally so smaller crops will be more frequent than large ones (since theres more possible translations with larger zooms)
-    M /= n_step
-
-    # convert each cache entry into a tensor of flattened indices
-    cache_tensor = torch.zeros((h * w, n_step), dtype=torch.int32)
-    for k in cache:
-        tens = torch.tensor(cache[k])
-        assert tens.shape[0] == n_step
-        cache_tensor[k[0] * w + k[1]] = tens[:, 0] * w + tens[:, 1]
-
-    # M is the same across all channels (and channels are independent of one another)
-    return torch.block_diag(*[M for _ in range(img_shape[0])]), n_step, cache_tensor
-
-
+from augmentation import RandomCrop, HorizFlip, Grayscale, ColorJitter
 
 
 def generate_aug_probs(img_shape):
     with torch.no_grad():    
         zoom_factors = [1.25, 2, 3]
-        rc_op, rc_nstep, rc_cache = resize_crop_operator(img_shape, zoom_factors)
-        return {"resize_crop" : rc_op, "resize_nstep" : rc_nstep, "resize_cache" : rc_cache}
+        return {
+            "rc" : RandomCrop(img_shape, zoom_factors),
+            # "horiz" : HorizFlip(0.5),
+            # "gray" : Grayscale(0.2),
+            # "jitter" : ColorJitter([0.4, 0.4, 0.2, 0.1], 0.8)
+        }
 
 
 def calc_aug_ev_var(x, prob_map):
@@ -147,51 +44,11 @@ def calc_aug_ev_var(x, prob_map):
         - Bernoulli var. of grayscale application
     """
 
-    def random_resized_crop(x):
-        """
-        Scale: [lower, upper] bound on the ratio of the original height/width of the crop prior to resizing (NOTE: RandomResizedCrop uses area + non-square crops)
-        
-        2) Select + perform horizontal/vertical translations 
-        3) Perform zoom to achieve the selected crop size
-        """
-        
-
-
-        # TODO(as) this is stupid. discretize the zoom. Then just define a uniform prob. dist over all possible crops (Cart. prod. of all sizes with all valid horiz/vert translations)
-        # Then, iterate through all these crops and determine which source pixel each final pixel comes from (+ associate appropriate prob. mass to it)
-
-        M = prob_map["resize_crop"].to(x.device).to(x.dtype)
-
-        # mean image
-        flat = x.flatten(1).T
-        EM = (M @ flat).T
-        
-        # calculate augmentation variance
-        cache, nstep = prob_map["resize_cache"], prob_map["resize_nstep"]
-        
-        # for all ((u, v), (u', v')): integrate over all ((x, y), (x', y')) pairs where t((x, y)) == (u, v) and t((x', y')) == (u', v')
-        # sum over all elements in the outer product of x[..., cache] with itself along its final dimension
-        x = x.flatten(2)
-        slc = x[..., cache]
-        second_mom = torch.einsum('bchw,bcdk->bchd', slc, slc)
-
-        # scale by prob. of each possible augmentation
-        second_mom /= nstep
-
-        # var = second moment - EM^2
-        # assumes all channels are independent
-        EM_tmp = einops.rearrange(EM, "a (b c) -> a b c", b=second_mom.shape[1])
-        second_mom -= (EM_tmp.unsqueeze(-2) * EM_tmp.unsqueeze(-1))
-
-        return EM, second_mom
-
-    ev, rrc_var = random_resized_crop(x)
-    var = rrc_var
-    
-
     # TODO(as) apply horiz flip, grayscale, + jitter bernoulli augmentations
 
-
+    ev, rrc_var = prob_map["rc"].calc_mean_var(x)
+    var = rrc_var
+    
 
     # EV needs to be in a format to go through the network
     ev = einops.rearrange(ev, "B (C H W) -> B C H W", C=x.shape[1], H=x.shape[2])
@@ -208,23 +65,6 @@ def calc_aug_ev_var(x, prob_map):
 
 
 
-
-
-def calc_model_jac(model, inp):
-    def helper(x):
-        _, out = model(x)
-        return out.squeeze()
-
-    # TODO(as) sketchy... means running stats wont be updated
-    model.eval()
-    J = torch.func.vmap(torch.func.jacrev(helper))(inp)
-
-    # J = torch.zeros((inp.shape[0], 16, *inp.shape[1:]), device=inp.device, dtype=inp.dtype)
-    # for idx in range(inp.shape[0]):
-    #     J[idx] = torch.func.jacrev(helper)(inp[idx])
-
-    model.train()
-    return J
 
 
 def calc_tangent_prop_loss(model, inp, var_decomp):
@@ -307,12 +147,13 @@ def log_model_jacobian(vis_dict, stats_data, model, device):
     jac_norm_sum = 0
     batch_sz = 16
 
+    def helper(model, x):
+        return model(x)[1].squeeze()
+
     for start in range(0, stats_data.shape[0], batch_sz):
         end = min(start + batch_sz, stats_data.shape[0])
         btch = stats_data[start : end].unsqueeze(1).to(device)
-        jac = calc_model_jac(model, btch)
-        jac = jac.flatten(1, -1)
-
+        jac = torch.func.vmap(torch.func.jacrev(helper))(model, btch).flatten(1, -1)
         jac_norm_sum += torch.linalg.norm(jac, dim=1).sum()
 
     # mean of per-sample Jacobian norms
---
+++ mmcr/cifar_stl/augmentation.py
@@ -0,0 +1,213 @@
+import torch
+import torchvision
+from tqdm import tqdm
+import einops
+
+import pdb
+
+
+class RandomCrop():
+    def __init__(self, img_shape, zoom_factors):
+        def _per_crop_prob_calc(M, zf, horiz_step, vert_step, h, w):
+
+            # for u in range(h):
+            #     for v in range(w):
+            #         # (u, v) is (row, col) destination crop indexing. (x, y) is (row, col) indexing of the source pixel in the original image
+            #         x = int((u + vert_step) / zf)
+            #         y = int((v + horiz_step) / zf)
+            #         m_source_idx = x * w + y
+
+            #         m_idx = u * w + v
+            #         local_M[m_idx, m_source_idx] += 1
+
+            #         for u_prime in range(h):
+            #             for v_prime in range(w):
+            #                 x_prime = int((u_prime + vert_step) / zf)
+            #                 y_prime = int((v_prime + horiz_step) / zf)
+            #                 prime_idx = x_prime * w + y_prime
+            #                 local_var[m_source_idx, prime_idx] += 1
+
+            # return local_M, local_var
+
+            # calculate the source pixel coordinates from: destination pixel coordinates, zoom amount, + horiz/vert translation
+            local_M = torch.zeros_like(M)
+            u_grid, v_grid = torch.meshgrid(torch.arange(h), torch.arange(w), indexing='ij')
+            x = ((u_grid + vert_step) / zf).int()
+            y = ((v_grid + horiz_step) / zf).int()
+            m_source_idx = (x * w + y).reshape(-1)
+            m_dest_idx = (u_grid * w + v_grid).reshape(-1)
+            
+            # NOTE: can do assignment without accumulation since each m_dest_idx is unique. this implies that each (dest, source) pair is also unique
+            local_M[m_dest_idx, m_source_idx] = 1
+
+            # Outer product of counts with itself gives us the number of times each (m_source_idx, m_source_idx) pair appears
+            # counts = torch.bincount(m_source_idx, minlength=var.shape[0])
+            # local_var = torch.outer(counts, counts)
+
+            return local_M
+
+        
+        # Discretized zoom (discretized crop sizes) allow the definition of a discrete uniform probability distribution over all crop size and horiz/vert translation pairs
+        h, w = img_shape[-2], img_shape[-1]
+        sz = h * w
+        M = torch.zeros((sz, sz)) 
+
+        cache = {}
+        for u in range(h):
+            for v in range(w):
+                cache[(u, v)] = []
+
+        # TODO(as) this is stupid. discretize the zoom. Then just define a uniform prob. dist over all possible crops (Cart. prod. of all sizes with all valid horiz/vert translations)
+        # Then, iterate through all these crops and determine which source pixel each final pixel comes from (+ associate appropriate prob. mass to it)
+        n_step = 0
+        for zf in zoom_factors:
+            # determine zoom size and then find all windows of original image size that will work as a crop
+            zoom_h, zoom_w = int(zf * h), int(zf * w)
+            n_horiz_step = zoom_w - w + 1
+            n_vert_step = zoom_h - h + 1
+
+            # iterate through all possible translations of this size of crop (assigning each uniform probability)
+            for horiz_step in tqdm(range(n_horiz_step)):
+                for vert_step in range(n_vert_step):
+                    n_step += 1
+                    # determine the source image pixel that corresponds to each output crop pixel and update M
+                    local_M = _per_crop_prob_calc(M, zf, horiz_step, vert_step, h, w)
+                    M += local_M
+
+                    # prep (u, v) -> (x, y) cache for variance calculation
+                    for u in range(w):
+                        for v in range(h):
+                            x = int((u + vert_step) / zf)
+                            y = int((v + horiz_step) / zf)
+                            cache[(u, v)].append((x, y))
+
+
+
+        # normalize M values by the total number of possible (crop size, horiz/vert translation) combinations
+        # NOTE: this weights all crop sizes equally so smaller crops will be more frequent than large ones (since theres more possible translations with larger zooms)
+        M /= n_step
+
+        # convert each cache entry into a tensor of flattened indices
+        cache_tensor = torch.zeros((h * w, n_step), dtype=torch.int32)
+        for k in cache:
+            tens = torch.tensor(cache[k])
+            assert tens.shape[0] == n_step
+            cache_tensor[k[0] * w + k[1]] = tens[:, 0] * w + tens[:, 1]
+
+        # M is the same across all channels (and channels are independent of one another)
+        self.op = torch.block_diag(*[M for _ in range(img_shape[0])])
+        self.nstep = n_step
+        self.cache = cache_tensor
+        self.zf = zoom_factors
+    
+
+    def calc_mean_var(self, x):
+        """
+        Scale: [lower, upper] bound on the ratio of the original height/width of the crop prior to resizing (NOTE: RandomResizedCrop uses area + non-square crops)
+        
+        2) Select + perform horizontal/vertical translations 
+        3) Perform zoom to achieve the selected crop size
+        """
+
+        # TODO(as) this is stupid. discretize the zoom. Then just define a uniform prob. dist over all possible crops (Cart. prod. of all sizes with all valid horiz/vert translations)
+        # Then, iterate through all these crops and determine which source pixel each final pixel comes from (+ associate appropriate prob. mass to it)
+
+        M = self.op.to(x.device).to(x.dtype)
+
+        # mean image
+        flat = x.flatten(1).T
+        EM = (M @ flat).T
+        
+        # calculate augmentation variance
+        
+        # for all ((u, v), (u', v')): integrate over all ((x, y), (x', y')) pairs where t((x, y)) == (u, v) and t((x', y')) == (u', v')
+        # sum over all elements in the outer product of x[..., cache] with itself along its final dimension
+        x = x.flatten(2)
+        slc = x[..., self.cache]
+        second_mom = torch.einsum('bchw,bcdk->bchd', slc, slc)
+
+        # scale by prob. of each possible augmentation
+        second_mom /= self.nstep
+
+        # var = second moment - EM^2
+        # assumes all channels are independent
+        EM_tmp = einops.rearrange(EM, "a (b c) -> a b c", b=second_mom.shape[1])
+        second_mom -= (EM_tmp.unsqueeze(-2) * EM_tmp.unsqueeze(-1))
+
+        return EM, second_mom
+
+
+    def generate_random_sample(self, x):
+        # Randomly sample a zoom factor, then randomly sample a vertical/horizontal translation. Resize and return.
+        pass
+
+
+
+class BernoulliAug():
+    def __init__(self, prob):
+        self.prob = prob
+
+    def _augment(self, x):
+        return None
+
+    def calc_mean_var(self, x, prob):
+        aug = self._augment(x)
+        ev = prob * aug + (1 - prob) * x
+
+        # covariance matrix is N x N (where N is the size of the flattened image)
+        second_mom_diag = prob * (aug ** 2) + (1 - prob) * (x ** 2)         # E[X^2]
+        var_diag = second_mom_diag - (ev ** 2)                              # E[X^2] - E[X]^2
+
+        # in the bernoulli case all pixels are independent of one another so covariance matrix is diagonal
+        var = torch.diag(var_diag.flatten())
+        return ev, var
+    
+    def generate_random_sample(self, x):
+        pass
+            
+class HorizFlip(BernoulliAug):
+    def __init__(self, prob):
+        super().__init__(prob)
+
+    def _augment(self, x):
+        return torchvision.transforms.functional.hflip(x)
+
+class Grayscale(BernoulliAug):
+    def __init__(self, prob):
+        super().__init__(prob)
+        self.gs = torchvision.transforms.Grayscale(3)
+
+    def _augment(self, x):
+        return self.gs(x)
+
+class ColorJitter():
+    def __init__(self, param, prob):
+        # ColorJitter samples uniformly at random from range for each. Specify size 0 range to get deterministic behavior
+        mx = [(j, j) for j in [i + 1 for i in param[:-1]]] + [(param[-1], param[-1])]
+        mn = [(j, j) for j in [max(0, 1 - i) for i in param[:-1]]] + [(-1 * param[-1], -1 * param[-1])]
+        
+        self.mx_jitter = torchvision.transforms.ColorJitter(*mx)
+        self.mn_jitter = torchvision.transforms.ColorJitter(*mn)
+        
+        self.param = param
+        self.prob = prob
+
+    def calc_mean_var(self, x):
+        
+        # TODO(as) this is wrong:
+        #   - need Bernoulli over entire transform
+        #   - need EV to be mean jitter value (0.5 * (mx - mn) + mn)
+        #   - variance calc. is wrong. this is a joint over uniform distributions, not one big uniform dist
+        
+        # variance of the uniform distribution ((b - a)^2) / 12 where b and a are the limits (per-pixel in our case)
+        x_mx_jitter = self.mx_jitter(x)
+        x_mn_jitter = self.mn_jitter(x)
+        var_diag = ((x_mx_jitter - x_mn_jitter) ** 2) / 12
+
+        # NOTE: "mean" image is the average over all the jittered images... not necessarily an un-perturbed image
+        
+        return x, torch.diag(var_diag.flatten())
+
+    def generate_random_sample(self, x):
+        pass
+
