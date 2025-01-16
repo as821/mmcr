554b1c656b0b0f4922d685a7567dc188ab6d2ae7
as821 <andrew.r.stange@gmail.com>
2025-01-15 21:48:32 -0800

    nevermind

--- mmcr/cifar_stl/train.py
+++ mmcr/cifar_stl/train.py
@@ -112,7 +112,7 @@ def train(args):
             # forward pass
             # with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
             img_batch, labels = data_tuple
-            img_batch = einops.rearrange(img_batch, "B N C H W -> (B N) C H W").cuda()
+            img_batch = einops.rearrange(img_batch, "B N C H W -> (B N) C H W").cuda(non_blocking=True)
             _, out = model(img_batch)
             
             # calculate outer product of outputs projected to the unit circle (inner product of each pair of features), O(N^2)
